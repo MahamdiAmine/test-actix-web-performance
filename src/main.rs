c180d94f9554e61c2473da7c4268592bd744a7cb
Mahamdi <fm_mahamdi@esi.dz>
2022-01-21 23:39:49 +0100

    Test serialization performance by returning a json response

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,16 @@ async fn hello() -> impl Responder {
     format!("Hello Rusty!")
 }
 
+async fn hello_json() -> impl Responder {
+    web::Json("Hello Rusty-dusty")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(hello))
+            .route("/json", web::get().to(hello_json))
     })
     .bind(("0.0.0.0", 8080))?
     .run()
