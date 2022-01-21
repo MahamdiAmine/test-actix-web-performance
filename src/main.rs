0b5a2062c816871b13124b987973cb6bc9a50f07
Mahamdi <fm_mahamdi@esi.dz>
2022-01-21 23:31:47 +0100

    Basic route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer, Responder};
+
+async fn hello() -> impl Responder {
+    format!("Hello Rusty!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(hello))
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
 }
